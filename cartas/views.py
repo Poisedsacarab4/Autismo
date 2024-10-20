8c91cedb0c61fdd894c538586e4b6e589d4a0f7a
Jama347 <holajoaquin15@gmail.com>
2024-10-20 17:40:18 -0300

    al correrlo ahora muestra algo

--- cartas/views.py
+++ cartas/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+def hola(request):
+    return HttpResponse("<h1>hola chavales del youtube</h1>")
+
