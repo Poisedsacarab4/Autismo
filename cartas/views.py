caeeceeb63ac7dbe68c697f7ecfecc627879690d
Poisedsacarab4 <minescarab.d@gmail.com>
2024-10-21 15:29:16 -0300

    :v
    :v

--- cartas/views.py
+++ cartas/views.py
@@ -4,5 +4,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 def hola(request):
-    return HttpResponse("<h1>hola chavales del youtube</h1>")
+    return HttpResponse("<h1>hola chavales del youtube :v</h1><h2>que gei</h2><h3>hola</h3>")
 
