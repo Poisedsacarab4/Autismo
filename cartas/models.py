f9725e20f86a75e1230ea9012280a7923fcf0b5e
Jama347 <holajoaquin15@gmail.com>
2024-11-29 12:09:24 -0300

    version final pre-presentacion (funciona todo)

--- cartas/models.py
+++ cartas/models.py
@@ -5,10 +5,6 @@ class Carta(models.Model):
     titulo = models.CharField(max_length=100)
     contenido = models.TextField()
     autor = models.ForeignKey(User, on_delete=models.CASCADE)
-    ## Nuevo por dieguito :3
-    Order = models.IntegerField()
-    Type = models.IntegerField()
-    date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.titulo
